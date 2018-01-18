98a0df1a378ac25632c646679b24af116775c74d
Shikirra Williams <shikirra.williams@gmail.com>
2018-01-17 23:07:27 -0500

    made adjustments to bio section, stylized

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-// $(document).ready(function () {
-//     $('#sidebarCollapse').on('click', function () {
-//         $('#sidebar').toggleClass('active');
-//         $(this).toggleClass('active');
-//     });
-// });
+
